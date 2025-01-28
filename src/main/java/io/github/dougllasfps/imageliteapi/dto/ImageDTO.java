64358809312e7f92b176b42186a413a3d8b04836
Lucas williams <lucaswilliamscesar@gmail.com>
2025-01-28 20:31:37 -0300

    created ImageDTO

---
+++ src/main/java/io/github/dougllasfps/imageliteapi/dto/ImageDTO.java
@@ -0,0 +1,17 @@
+package io.github.dougllasfps.imageliteapi.dto;
+
+import java.time.LocalDate;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ImageDTO {
+
+	private String url;
+	private String name;
+	private String extension;
+	private Long size;
+	private LocalDate uploadDate;
+}
